d92cb1d6def13518812894ccb422743b46175c2f
sampathvenur <sampathkumarvenur@gmail.com>
2026-02-11 21:32:51 +0530

    project: Task 1: Project Setup & Foundation

---
+++ project/js/app.js
@@ -0,0 +1,4 @@
+import { category, priorityLevels, appName, PageSize, darkMode } from "./config.js";
+
+console.log(category);
+console.log(appName);
---
+++ project/js/config.js
@@ -0,0 +1,14 @@
+export const category = ['Work', 'Personal', 'Learning', 'Health'];
+
+export const priorityLevels = {
+    1: 'Low',
+    2: 'Medium',
+    3: 'High',
+    4: 'Critical'
+};
+
+export const appName = 'TaskFlow';
+
+export const PageSize = 10;
+
+export const darkMode = false;
